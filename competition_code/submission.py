de3b6a9abb0ec4a630355ed285f0ddc9b7669a28
Emma Sun <emma.sun.0710@gmail.com>
2025-08-28 23:45:28 -0700

    submit& C:/Users/sunch/miniconda3/envs/roar_comp/python.exe c:/Users/sunch/Project/roar_competition_new/ROAR_Competition/competition_code/competition_runner.py

--- competition_code/submission.py
+++ competition_code/submission.py
@@ -3,12 +3,15 @@ Competition instructions:
 Please do not change anything else but fill out the to-do sections.
 """
 
+
 from typing import List, Tuple, Dict, Optional
 import roar_py_interface
 import numpy as np
 
+
 def normalize_rad(rad : float):
     return (rad + np.pi) % (2 * np.pi) - np.pi
+
 
 def filter_waypoints(location : np.ndarray, current_idx: int, waypoints : List[roar_py_interface.RoarPyWaypoint]) -> int:
     def dist_to_waypoint(waypoint : roar_py_interface.RoarPyWaypoint):
@@ -19,6 +22,7 @@ def filter_waypoints(location : np.ndarray, current_idx: int, waypoints : List[r
         if dist_to_waypoint(waypoints[i%len(waypoints)]) < 3:
             return i % len(waypoints)
     return current_idx
+
 
 class RoarCompetitionSolution:
     def __init__(
@@ -40,15 +44,17 @@ class RoarCompetitionSolution:
         self.rpy_sensor = rpy_sensor
         self.occupancy_map_sensor = occupancy_map_sensor
         self.collision_sensor = collision_sensor
-    
+   
     async def initialize(self) -> None:
         # TODO: You can do some initial computation here if you want to.
         # For example, you can compute the path to the first waypoint.
 
-        # Receive location, rotation and velocity data 
+
+        # Receive location, rotation and velocity data
         vehicle_location = self.location_sensor.get_last_gym_observation()
         vehicle_rotation = self.rpy_sensor.get_last_gym_observation()
         vehicle_velocity = self.velocity_sensor.get_last_gym_observation()
+
 
         self.current_waypoint_idx = 10
         self.current_waypoint_idx = filter_waypoints(
@@ -56,6 +62,8 @@ class RoarCompetitionSolution:
             self.current_waypoint_idx,
             self.maneuverable_waypoints
         )
+
+
 
 
     async def step(
@@ -68,12 +76,13 @@ class RoarCompetitionSolution:
         """
         # TODO: Implement your solution here.
 
-        # Receive location, rotation and velocity data 
+
+        # Receive location, rotation and velocity data
         vehicle_location = self.location_sensor.get_last_gym_observation()
         vehicle_rotation = self.rpy_sensor.get_last_gym_observation()
         vehicle_velocity = self.velocity_sensor.get_last_gym_observation()
         vehicle_velocity_norm = np.linalg.norm(vehicle_velocity)
-        
+       
         # Find the waypoint closest to the vehicle
         self.current_waypoint_idx = filter_waypoints(
             vehicle_location,
@@ -81,31 +90,40 @@ class RoarCompetitionSolution:
             self.maneuverable_waypoints
         )
          # We use the 3rd waypoint ahead of the current waypoint as the target waypoint
-        waypoint_to_follow = self.maneuverable_waypoints[(self.current_waypoint_idx + 3) % len(self.maneuverable_waypoints)]
+        idx_off = 5 if vehicle_velocity_norm > 20.0 else (4 if vehicle_velocity_norm > 14.0 else 3)
+        waypoint_to_follow = self.maneuverable_waypoints[(self.current_waypoint_idx + idx_off) % len(self.maneuverable_waypoints)]
+
 
         # Calculate delta vector towards the target waypoint
         vector_to_waypoint = (waypoint_to_follow.location - vehicle_location)[:2]
         heading_to_waypoint = np.arctan2(vector_to_waypoint[1],vector_to_waypoint[0])
 
+
         # Calculate delta angle towards the target waypoint
         delta_heading = normalize_rad(heading_to_waypoint - vehicle_rotation[2])
 
+
         # Proportional controller to steer the vehicle towards the target waypoint
         steer_control = (
-            -8.0 / np.sqrt(vehicle_velocity_norm) * delta_heading / np.pi
+            -6.5 / np.sqrt(vehicle_velocity_norm) * delta_heading / np.pi
         ) if vehicle_velocity_norm > 1e-2 else -np.sign(delta_heading)
         steer_control = np.clip(steer_control, -1.0, 1.0)
 
+
         # Proportional controller to control the vehicle's speed towards 40 m/s
-        throttle_control = 0.05 * (20 - vehicle_velocity_norm)
+        throttle_control = 0.05 * (36 - vehicle_velocity_norm)
+
 
         control = {
             "throttle": np.clip(throttle_control, 0.0, 1.0),
             "steer": steer_control,
-            "brake": np.clip(-throttle_control, 0.0, 1.0),
+            "brake": np.clip(-throttle_control, 0.0, 0.35),
             "hand_brake": 0.0,
             "reverse": 0,
             "target_gear": 0
         }
         await self.vehicle.apply_action(control)
         return control
+
+
+
